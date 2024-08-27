e1a6ed228df65fad4ff2066d6ef264434ed92ccf
Austin <lyvuaustin@gmail.com>
2024-08-27 02:36:53 -0500

    Added simple basics for Step 2

---
+++ Step 2/main.py
@@ -0,0 +1,44 @@
+# Main Features
+# --------------------------------------------- Income Tracker --------------------------------------------- #
+
+# Log sources of Incomes
+income_input = input("Please enter you weekly income: ")
+income_option_input = input("Please enter the type of income (Salary, Freelance, etc...): ")
+income_date_input = input("Please enter date of income (Leave blank for Today): ")
+
+# --------------------------------------------- Expense Tracker --------------------------------------------- #
+
+# Log sources of Expenses
+expense_input = input("Please enter expense: ")
+expense_type_input = input("Please enter expense type (Rent, groceries, entertainment, etc...): ")
+expense_date_input = input("Please enter date of expense (Leave blank for Today): ")
+
+# --------------------------------------------- Saving Goals --------------------------------------------- #
+
+# Log Savings Goals
+savings_input = input("Please enter what you Savings Goal is: ")
+savings_type_input = input("Please enter the type of Savings Goal (Vacation, Emergencies, etc...): ")
+savings_date_input = input("Please enter date of Savings Goal (Leave blank for Today): ")
+
+# --------------------------------------------- Reporting --------------------------------------------- #
+
+# Create visual report of income vs expenses
+
+# Create Visual report of Category-wise expense break down
+
+# Create visual report of Progress towards savings goal
+
+### Optional Features
+# --------------------------------------------- Budgeting --------------------------------------------- #
+
+# Set monthly budget (Maybe for different categories and alerts when exceeding)
+monthly_budget_input = input("Please enter the monthly budget: ")
+monthly_budget_type_input = input("Please enter the category for the budget (Leave blank if overall budget): ")
+
+# --------------------------------------------- Recurring Transactions --------------------------------------------- #
+if expense_date == expense_date + 7:
+
+    if expense_name == expense_name:
+
+        pass
+        # Then log recurring payments somewhere
