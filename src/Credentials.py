d05a8da9f859a8eac1749886c96dcc20efb153a4
Austin <lyvuaustin@gmail.com>
2024-09-23 14:14:32 -0500

    Added Credential.py with placeholder values

---
+++ src/Credentials.py
@@ -0,0 +1,7 @@
+# ----------------------------- Database Credentials ----------------------------- #
+
+DATABASE_NAME = "Your_Database_Name"
+USERNAME = "Your_Username"
+PASSWORD = "Your_Password"
+HOST = "Your Host"
+PORT = 5432 # Default Port Number
